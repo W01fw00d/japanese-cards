aaadb61ad9aac730fbb69508107c262d74f3e9f9
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-05-25 19:28:57 +0900

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Card } from "antd";
+import { Button } from "antd";
+
+import "./App.css";
+
+//import csv from "./class1_numbers.csv";
+import parser from "./cvsToJsonParser.js";
 
 function App() {
+  const [cards, setCards] = React.useState([]);
+  const [currentCard, setCurrentCard] = React.useState(0);
+  const [currentLanguage, setCurrentLanguage] = React.useState("japanese");
+
+  const fr = new FileReader();
+  fr.onload = function () {
+    const json = parser(fr.result);
+    setCards(JSON.parse(json));
+  };
+
+  React.useEffect(() => {
+    document.getElementById("fileupload").addEventListener(
+      "change",
+      function (e) {
+        var files = e.target.files;
+        fr.readAsText(files[0]);
+      },
+      false
+    );
+  }, [fr]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="form-group">
+          <div>
+            <label>Upload a CSV formatted file:</label>
+          </div>
+          <input
+            type="file"
+            id="fileupload"
+            class="form-control"
+            accept=".csv"
+            required
+          />
+        </div>
+        <div id="output"></div>
+        {cards && cards.length > 0 && (
+          <Card
+            title={
+              <Button
+                type="primary"
+                onClick={() => {
+                  setCurrentLanguage(
+                    currentLanguage === "japanese" ? "english" : "japanese"
+                  );
+                }}
+              >
+                Flip
+              </Button>
+            }
+            extra={
+              <Button
+                type="primary"
+                onClick={() => {
+                  setCurrentLanguage("japanese");
+                  setCurrentCard(currentCard + 1);
+                }}
+              >
+                Next
+              </Button>
+            }
+            style={{ width: 300 }}
+          >
+            <p>{cards[currentCard][currentLanguage]}</p>
+          </Card>
+        )}
       </header>
     </div>
   );
---
+++ src/cvsToJsonParser.js
@@ -0,0 +1,35 @@
+//var csv is the CSV file with headers
+export default (csv) => {
+  console.log("csv", csv);
+  var lines = csv.split("\n");
+
+  var result = [];
+
+  // NOTE: If your columns contain commas in their values, you'll need
+  // to deal with those before doing the next step
+  // (you might convert them to &&& or something, then covert them back later)
+  // jsfiddle showing the issue https://jsfiddle.net/
+  var headers = lines[0].split(",");
+
+  for (var i = 1; i < lines.length; i++) {
+    var obj = {};
+    var currentline = lines[i].split(",");
+
+    for (var j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j];
+    }
+
+    result.push(obj);
+  }
+
+  //return result; //JavaScript object
+  const mappedResult = result.map((row) => {
+    return {
+      japanese: row["\r"],
+      english: row.English,
+    };
+  });
+
+  //console.log("JS object: ", result, mappedResult);
+  return JSON.stringify(mappedResult); //JSON
+};

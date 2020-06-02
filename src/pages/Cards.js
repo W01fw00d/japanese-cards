f6cee17e69eebb7b5628ccabe9c078affef9c8ea
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-06-02 18:02:47 +0900

    Add Select in Cards Page to be able to choose jsons

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -3,16 +3,44 @@ import React, { useState } from "react";
 import { Card } from "antd";
 import { Button } from "antd";
 
+import { Select } from "antd";
+
 import "./Cards.css";
 
 import hiraganajson from "../jsons/May7th_voc.json";
-
-//import kanjiJson from "../jsons/Kanji Summary.json";
+import kanjiJson from "../jsons/Kanji Summary.json";
 
 export default () => {
-  const [cards] = useState(hiraganajson || []);
+  const { Option } = Select;
+
+  const vocabularyLists = {
+    hiragana1: hiraganajson,
+    kanji: kanjiJson,
+  };
+  const vocabularyListKeys = Object.keys(vocabularyLists);
+
+  const [currentVocabularyListKey, setCurrentVocabularyListKey] = useState(
+    vocabularyListKeys[0]
+  );
+  const [cards, setCards] = useState(vocabularyLists[vocabularyListKeys[0]]);
   const [currentCard, setCurrentCard] = useState(0);
   const [currentLanguage, setCurrentLanguage] = useState("japanese");
+
+  const VocabularyListSelect = () => (
+    <Select
+      defaultValue={currentVocabularyListKey}
+      style={{ width: 120 }}
+      onChange={(value) => {
+        setCurrentVocabularyListKey(value);
+        setCards(vocabularyLists[value]);
+        setCurrentCard(0);
+      }}
+    >
+      {vocabularyListKeys.map((key) => (
+        <Option value={key}>{key}</Option>
+      ))}
+    </Select>
+  );
 
   const LargeButton = ({ text, onClick, disabled }) => (
     <Button type="primary" size="large" disabled={disabled} {...{ onClick }}>
@@ -50,11 +78,12 @@ export default () => {
 
   return (
     <>
+      <VocabularyListSelect />
       {cards && cards.length > 0 && (
         <Card
           title={<FlipButton />}
           extra={<NextButton />}
-          style={{ width: 300 }}
+          style={{ width: 300, marginTop: "80px" }}
         >
           <p className="big-font">{cards[currentCard][currentLanguage]}</p>
         </Card>
